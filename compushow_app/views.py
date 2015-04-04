2d6212a9645bedb4683af6d904e063d613c46423
Sergio Teran <sergio@sergio>
2015-04-04 12:25:09 -0430

    Agrege la pagina de cada nominacion, cree un modelo index que es donde se deberia editar todo menos los botones de nombre y foto, esos estan dentro de cada nominacion, puse unas fotos pero eso lo pueden cambiar era para probar haha

--- compushow_app/views.py
+++ compushow_app/views.py
@@ -14,7 +14,55 @@ def login(request):
 		if form.is_valid():
 			carnet=form.cleaned_data['Carnet']
 			password=form.cleaned_data['Password']
-			return render_to_response('index.html',{'form':form},context_instance=RequestContext(request))
+			return render_to_response('CompuChill.html',{'form':form},context_instance=RequestContext(request))
 	else:
 		form = LoginForm()
 	return render_to_response('login.html',{'form':form},context_instance=RequestContext(request))
+
+def CompuChill(request):
+	return render_to_response('CompuChill.html',locals(),context_instance=RequestContext(request))
+
+def CompuGordito(request):
+	return render_to_response('CompuGordito.html',locals(),context_instance=RequestContext(request))
+
+def CompuProductista(request):
+	return render_to_response('CompuProductista.html',locals(),context_instance=RequestContext(request))
+
+def CompuCartoon(request):
+	return render_to_response('CompuCartoon.html',locals(),context_instance=RequestContext(request))
+
+def CompuComadre(request):
+	return render_to_response('CompuComadre.html',locals(),context_instance=RequestContext(request))
+
+def CompuCompadre(request):
+	return render_to_response('CompuCompadre.html',locals(),context_instance=RequestContext(request))
+
+def CompuLove(request):
+	return render_to_response('CompuLove.html',locals(),context_instance=RequestContext(request))
+
+def CompuCuchi(request):
+	return render_to_response('CompuCuchi.html',locals(),context_instance=RequestContext(request))
+
+def CompuIntenso(request):
+	return render_to_response('CompuIntenso.html',locals(),context_instance=RequestContext(request))
+
+def CompuPregunton(request):
+	return render_to_response('CompuPregunton.html',locals(),context_instance=RequestContext(request))
+
+def CompuFitness(request):
+	return render_to_response('CompuFitness.html',locals(),context_instance=RequestContext(request))
+
+def CompuTeam(request):
+	return render_to_response('CompuTeam.html',locals(),context_instance=RequestContext(request))
+
+def CompuMaster(request):
+	return render_to_response('CompuMaster.html',locals(),context_instance=RequestContext(request))
+
+def CompuPro(request):
+	return render_to_response('CompuPro.html',locals(),context_instance=RequestContext(request))
+
+def CompuPapi(request):
+	return render_to_response('CompuPapi.html',locals(),context_instance=RequestContext(request))
+
+def CompuMami(request):
+	return render_to_response('CompuMami.html',locals(),context_instance=RequestContext(request))
